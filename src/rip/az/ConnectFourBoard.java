69e5bdf18748f17373846411e9427e265df45a86
Alex Zvorygin <grafetu@gmail.com>
2022-04-23 16:09:22 -0700

    gitiginore, boardtests

--- src/rip/az/ConnectFourBoard.java
+++ src/rip/az/ConnectFourBoard.java
@@ -1,10 +1,7 @@
 package rip.az;
 
 import org.jetbrains.annotations.NotNull;
-import rip.az.LocationGenerator.DownAndRightLocationGenerator;
-import rip.az.LocationGenerator.LocationGenerator;
-import rip.az.LocationGenerator.RowLocationGenerator;
-import rip.az.LocationGenerator.UpAndRightLocationGenerator;
+import rip.az.LocationGenerator.*;
 
 import java.util.Arrays;
 
@@ -12,6 +9,7 @@ public class ConnectFourBoard implements Board {
     public static final LocationGenerator ROW_LOCATION_GENERATOR = new RowLocationGenerator();
     public static final LocationGenerator DOWN_AND_RIGHT_LOCATION_GENERATOR = new DownAndRightLocationGenerator();
     public static final LocationGenerator UP_AND_RIGHT_LOCATION_GENERATOR = new UpAndRightLocationGenerator();
+    public static final ColumnLocationGenerator COLUMN_LOCATION_GENERATOR = new ColumnLocationGenerator();
     private final int WIDTH = 7;
     private final int HEIGHT = 6;
     private final int CONNECT_AMOUNT = 4;
@@ -22,6 +20,7 @@ public class ConnectFourBoard implements Board {
 
     public ConnectFourBoard() {
         this.board = new Player[WIDTH * HEIGHT];
+        Arrays.fill(this.board, Player.NONE);
         this.turn = 0;
         this.heights = new int[WIDTH];
     }
@@ -33,6 +32,9 @@ public class ConnectFourBoard implements Board {
     }
 
     public Player getContentsAt(int column, int row) {
+        if (isOutOfBounds(column, row)) {
+            throw new IllegalArgumentException("Attempting to access OutOfBounds content. Column: " + column + ", row: " + row);
+        }
         return board[rowColumnToBoardIndex(column, row)];
     }
 
@@ -50,25 +52,43 @@ public class ConnectFourBoard implements Board {
         return new ConnectFourBoard(newBoard, turn, heights);
     }
 
+    @Override
+    public String toString() {
+        String boardString = "0123456\n";
+        for (int row = HEIGHT - 1; row >= 0; row--) {
+            for (int column = 0; column < WIDTH; column++) {
+                Player contentsAt = getContentsAt(column, row);
+                boardString += switch (contentsAt) {
+                    case NONE -> ".";
+                    case ONE -> "o";
+                    case TWO -> "x";
+                };
+            }
+            boardString += '\n';
+        }
+        return "Turn: " + turn + "\n" + boardString;
+    }
+
+    public void applyMoveSeries(int[] moves) {
+        for (int move : moves) {
+            applyMove(new ConnectFourMove(move));
+        }
+    }
+
     public void applyMove(@NotNull ConnectFourMove move) {
         int column = move.getColumn();
         int row = columnToOpenSpot(column);
 
-        if (row == HEIGHT) {
-            System.err.println("Placing tile on column that's topped out. Column: " + column);
-            System.err.println(this.toString());
-            throw new IllegalStateException();
-        }
-        if (getContentsAt(column, row) != Player.NONE) {
-            System.err.println("Placing tile on spot that's already filled. Column: " + column);
-            System.err.println(this.toString());
-            throw new IllegalStateException();
+        if (isOutOfBounds(column, row)) {
+            System.err.println("Placing tile in out of bounds. Column: " + column + " Row: " + row);
+            System.err.println(this);
+            throw new IllegalArgumentException();
         }
 
         Player newContents = Player.getTurn(turn);
         turn++;
         board[rowColumnToBoardIndex(column, row)] = newContents;
-        heights[row]++;
+        heights[column]++;
     }
 
     @Override
@@ -86,6 +106,9 @@ public class ConnectFourBoard implements Board {
         }
 
         for (int column = 0; column < WIDTH; column++) {
+            result = evalSeries(column, 0, COLUMN_LOCATION_GENERATOR);
+            if (result != Player.NONE) return result;
+
             result = evalSeries(column, HEIGHT - 1, DOWN_AND_RIGHT_LOCATION_GENERATOR);
             if (result != Player.NONE) return result;
 
---
+++ src/rip/az/ConnectFourBoardTest.java
@@ -0,0 +1,73 @@
+package rip.az;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConnectFourBoardTest {
+    ConnectFourBoard board = new ConnectFourBoard();
+
+    @org.junit.jupiter.api.Test
+    void applyMove() {
+        board.applyMoveSeries(new int[]{
+                0, 0, 0, 0, 0, 0
+        });
+        assertThrows(IllegalArgumentException.class, () -> board.applyMoveSeries(new int[]{
+                0, 0, 0, 0, 0, 0
+        }));
+    }
+
+    private void applyMoveSequenceString(String moveSequence) {
+        int[] moves = new int[moveSequence.length()];
+        for (int index = 0; index < moveSequence.length(); index++) {
+            int column = moveSequence.charAt(index) - '0';
+            moves[index] = column;
+        }
+        board.applyMoveSeries(moves);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "", // empty board
+            "001122", // row
+            "010101", // column
+
+    })
+    void getWinnerNoWinner(String moveSequence) {
+        applyMoveSequenceString(moveSequence);
+        assertEquals(Player.NONE, board.getWinner());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "0101010", // starting from 0,0 row
+            "00112233", // starting from 0,0 column
+            "01122323303", // starting from 0,0 up and to the right
+            "012036261615050", // down and to the right
+            "23232323", // starting from 2,0 row
+            "22334455", // starting from 2,0 column
+            "23344545525", // starting from 2,0 up and to the right
+    })
+    void getWinnerPlayerOneWinner(String moveSequence) {
+        applyMoveSequenceString(moveSequence);
+        assertEquals(Player.ONE, board.getWinner(), "Expected player one to win in \n" + board.toString());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "60101010", // starting from 0,0 row
+            "600112233", // starting from 0,0 column
+            "601122323303", // starting from 0,0 up and to the right
+            "6012036261615050", // down and to the right
+            "623232323", // starting from 2,0 row
+            "622334455", // starting from 2,0 column
+            "623344545525", // starting from 2,0 up and to the right
+    })
+    void getWinnerPlayerTwoWinner(String moveSequence) {
+        applyMoveSequenceString(moveSequence);
+        assertEquals(Player.TWO, board.getWinner(), "Expected player two to win in \n" + board.toString());
+    }
+
+}
